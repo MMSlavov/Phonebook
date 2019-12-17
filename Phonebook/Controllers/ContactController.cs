736d59aa876d1b1c0bf4793587fc142463d581af
Mario Slavov <mmslavov00@gmail.com>
2019-12-17 23:22:22 +0200

    Phonebook project

---
+++ Phonebook/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phonebook.Data.Models;
+
+namespace Phonebook.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create(Contact contact)
+        {
+            if (contact.Name != null && contact.Number != null)
+            {
+                DataAccess.Contacts.Add(contact);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Phonebook/Data/Models/DataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phonebook.Data.Models
+{
+    public class DataAccess
+    {
+        public static List<Contact> Contacts { get; set; } = new List<Contact>();
+    }
+}
